3cf272850d52b19b6620df65db56285ba655ff60
renu100 <renu.chandraker08@gmail.com>
2021-09-16 12:54:20 +0530

    Calculate wages of total working hours or month

--- EmpWage.java
+++ EmpWage.java
@@ -5,17 +5,17 @@ public class EmpWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
-		int empHrs = 0;
-		int empWage = 0, totalEmpWage = 0;
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int empWage = 0, totalEmpWage = 0, empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 			switch ((int) empCheck) {
 			case IS_FULL_TIME:
 				empHrs = 8;
 				break;
-				
 			case IS_PART_TIME:
 				empHrs = 4;
 				break;
@@ -23,10 +23,10 @@ public class EmpWage {
 				empHrs = 0;
 				break;
 			}
+			totalEmpHrs += empHrs;
 			empWage = empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage += empWage;
 		}
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
-
 }
