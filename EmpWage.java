473856e3e862474bf362c18ad0da524a620ed249
renu100 <renu.chandraker08@gmail.com>
2021-09-16 12:25:44 +0530

    Sloving using Switch Case Statement

--- EmpWage.java
+++ EmpWage.java
@@ -1,23 +1,27 @@
 package com.fellowship;
 
 public class EmpWage {
-	static final int IS_FULL_TIME = 2;
-	static final int IS_PART_TIME = 1;
-	static final int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+	public static final int EMP_RATE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
 		int empHrs = 0;
 		int empWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME)
+		switch ((int) empCheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
+			break;
+		case IS_PART_TIME:
 			empHrs = 4;
-		else
+			break;
+		default:
 			empHrs = 0;
+			break;
+		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: " + empWage);
-
 	}
 
 }
